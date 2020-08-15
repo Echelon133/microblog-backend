6c79cd36b82d8bae0cd1490aaaf36e467077ee26
Echelon133 <echelon133@gmail.com>
2020-08-15 17:21:20 +0200

    Add custom queries that let users follow/unfollow eachother and list who they follow and who follows them

--- src/main/java/ml/echelon133/blobb/user/UserRepository.java
+++ src/main/java/ml/echelon133/blobb/user/UserRepository.java
@@ -1,10 +1,34 @@
 package ml.echelon133.blobb.user;
 
+import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends Neo4jRepository<User, UUID> {
     Optional<User> findByUsername(String username);
+
+    @Query( "MATCH (u1:User), (u2:User) " +
+            "WHERE u1.uuid = $uuidOfFollower AND u2.uuid = $uuidOfFollowed " +
+            "CREATE (u1)-[f:FOLLOWS]->(u2) " +
+            "RETURN id(f)")
+    Optional<Long> followUserWithUuid(UUID uuidOfFollower, UUID uuidOfFollowed);
+
+    @Query( "MATCH (u1:User)-[f:FOLLOWS]->(u2:User) " +
+            "WHERE u1.uuid = $uuidOfFollower AND u2.uuid = $uuidOfFollowed " +
+            "DELETE f")
+    void unfollowUserWithUuid(UUID uuidOfFollower, UUID uuidOfFollowed);
+
+    @Query( "MATCH (u:User)-[:FOLLOWS]->(followed) " +
+            "WHERE u.uuid = $uuid " +
+            "RETURN followed")
+    List<User> findAllFollowedByUserWithUuid(UUID uuid);
+
+    @Query( "MATCH (following)-[:FOLLOWS]->(u:User) " +
+            "WHERE u.uuid = $uuid " +
+            "RETURN following")
+    List<User> findAllFollowingUserWithUuid(UUID uuid);
+
 }

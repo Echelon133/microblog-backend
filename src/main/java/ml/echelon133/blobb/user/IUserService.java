0dbdaa6a0c1d9e6e94cb6388419f800b89564fd6
Echelon133 <echelon133@gmail.com>
2020-08-17 15:16:14 +0200

    Add findByUuid method signature

--- src/main/java/ml/echelon133/blobb/user/IUserService.java
+++ src/main/java/ml/echelon133/blobb/user/IUserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.UUID;
 
 public interface IUserService {
+    User findByUuid(UUID uuid) throws UserDoesntExistException;
     boolean followUserWithUuid(User user, UUID followUuid) throws UserDoesntExistException;
     boolean unfollowUserWithUuid(User user, UUID unfollowUuid) throws UserDoesntExistException;
     List<User> findAllFollowedBy(UUID uuid, Long skip, Long limit) throws UserDoesntExistException, IllegalArgumentException;

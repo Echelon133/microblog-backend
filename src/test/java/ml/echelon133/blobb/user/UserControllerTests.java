d01fc4f8083a14700120e88710b92ee4e8ae1ce6
Echelon133 <echelon133@gmail.com>
2020-08-18 16:54:50 +0200

    Implement tests of /api/users/{uuid}/profile endpoint

--- src/test/java/ml/echelon133/blobb/user/UserControllerTests.java
+++ src/test/java/ml/echelon133/blobb/user/UserControllerTests.java
@@ -39,6 +39,8 @@ public class UserControllerTests {
 
     private JacksonTester<User> jsonUser;
 
+    private JacksonTester<UserProfileInfo> jsonUserProfileInfo;
+
     @BeforeEach
     public void beforeEach() {
         JacksonTester.initFields(this, new ObjectMapper());
@@ -99,6 +101,66 @@ public class UserControllerTests {
         // when
         MockHttpServletResponse response = mockMvc.perform(
                 get("/api/users/" + uuid)
+                        .accept(APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        // then
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.getContentAsString()).isEqualTo(json.getJson());
+    }
+
+    @Test
+    public void getUserProfile_HandlesInvalidUuid() throws Exception {
+        String invalidUuid = "asdf";
+
+        // when
+        MockHttpServletResponse response = mockMvc.perform(
+                get("/api/users/" + invalidUuid + "/profile")
+                        .accept(APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        // then
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        assertThat(response.getContentAsString()).contains("Invalid UUID string");
+    }
+
+    @Test
+    public void getUserProfile_DoesntExist() throws Exception {
+        UUID uuid = UUID.randomUUID();
+
+        // given
+        given(userService.getUserProfileInfo(uuid))
+                .willThrow(new UserDoesntExistException(uuid));
+
+        // when
+        MockHttpServletResponse response = mockMvc.perform(
+                get("/api/users/" + uuid + "/profile")
+                        .accept(APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        // then
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.NOT_FOUND.value());
+        assertThat(response.getContentAsString())
+                .contains(String.format("User with UUID %s doesn't exist", uuid.toString()));
+    }
+
+    @Test
+    public void getUserProfile_ReturnsUserProfileInfo() throws Exception {
+        UUID uuid = UUID.randomUUID();
+        UserProfileInfo uProfileInfo = new UserProfileInfo();
+        uProfileInfo.setUuid(uuid);
+        uProfileInfo.setFollows(1L);
+        uProfileInfo.setFollowers(10L);
+
+        // expected json
+        JsonContent<UserProfileInfo> json = jsonUserProfileInfo.write(uProfileInfo);
+
+        // given
+        given(userService.getUserProfileInfo(uuid)).willReturn(uProfileInfo);
+
+        // when
+        MockHttpServletResponse response = mockMvc.perform(
+                get("/api/users/" + uuid + "/profile")
                         .accept(APPLICATION_JSON)
         ).andReturn().getResponse();
 

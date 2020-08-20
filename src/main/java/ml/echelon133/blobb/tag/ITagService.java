d26899178ce3f400591a0b36178628bf680bca87
Echelon133 <echelon133@gmail.com>
2020-08-20 16:54:54 +0200

    Fix method name

--- src/main/java/ml/echelon133/blobb/tag/ITagService.java
+++ src/main/java/ml/echelon133/blobb/tag/ITagService.java
@@ -3,6 +3,6 @@ package ml.echelon133.blobb.tag;
 import java.util.UUID;
 
 public interface ITagService {
-    Tag findById(UUID uuid) throws TagDoesntExistException;
+    Tag findByUuid(UUID uuid) throws TagDoesntExistException;
     Tag findByName(String name) throws TagDoesntExistException;
 }

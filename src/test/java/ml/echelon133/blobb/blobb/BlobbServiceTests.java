9e3485def99d5733fc95bcb75ede53407386b551
Echelon133 <echelon133@gmail.com>
2020-08-28 23:16:02 +0200

    Implement tests of getBlobbInfo

--- src/test/java/ml/echelon133/blobb/blobb/BlobbServiceTests.java
+++ src/test/java/ml/echelon133/blobb/blobb/BlobbServiceTests.java
@@ -53,4 +53,36 @@ public class BlobbServiceTests {
         // then
         assertEquals(uuid, fBlobb.getUuid());
     }
+
+    @Test
+    public void getBlobbInfo_ThrowsWhenBlobbDoesntExist() throws Exception {
+        UUID uuid = UUID.randomUUID();
+
+        // given
+        given(blobbRepository.getInfoAboutBlobbWithUuid(uuid)).willReturn(Optional.empty());
+
+        // then
+        String message = assertThrows(BlobbDoesntExistException.class, () -> {
+            blobbService.getBlobbInfo(uuid);
+        }).getMessage();
+
+        assertEquals(String.format("Blobb with UUID %s doesn't exist", uuid), message);
+    }
+
+    @Test
+    public void getBlobbInfo_ReturnsObject() throws Exception {
+        UUID uuid = UUID.randomUUID();
+
+        BlobbInfo info = new BlobbInfo();
+        info.setUuid(uuid);
+
+        // given
+        given(blobbRepository.getInfoAboutBlobbWithUuid(uuid)).willReturn(Optional.of(info));
+
+        // when
+        BlobbInfo receivedInfo = blobbService.getBlobbInfo(uuid);
+
+        // then
+        assertEquals(info, receivedInfo);
+    }
 }

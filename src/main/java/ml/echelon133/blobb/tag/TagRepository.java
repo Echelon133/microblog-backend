8a859c277b1e50b93ab3906e60e99feaf43e8de8
Echelon133 <echelon133@gmail.com>
2020-08-31 21:50:29 +0200

    Implement tag query that finds a list of the most popular tags between two dates

--- src/main/java/ml/echelon133/blobb/tag/TagRepository.java
+++ src/main/java/ml/echelon133/blobb/tag/TagRepository.java
@@ -1,10 +1,18 @@
 package ml.echelon133.blobb.tag;
 
+import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface TagRepository extends Neo4jRepository<Tag, UUID> {
     Optional<Tag> findByName(String name);
+
+    @Query( "MATCH (t:Tag)-[r:TAGS]->(b:Blobb) " +
+            "WHERE b.creationDate >= $first AND b.creationDate <= $second AND b.deleted <> true " +
+            "WITH t, count(r) as tagCounter RETURN t ORDER BY tagCounter DESC LIMIT $howManyTagsToGet")
+    List<Tag> findMostPopularTags_Between(Date first, Date second, Long howManyTagsToGet);
 }

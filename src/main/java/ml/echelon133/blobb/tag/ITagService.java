338dd03edb1bdc327d8a8409333336ccc02dc605
Echelon133 <echelon133@gmail.com>
2020-08-31 22:26:11 +0200

    Add declarations needed to implement findMostPopular

--- src/main/java/ml/echelon133/blobb/tag/ITagService.java
+++ src/main/java/ml/echelon133/blobb/tag/ITagService.java
@@ -1,8 +1,15 @@
 package ml.echelon133.blobb.tag;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ITagService {
+
+    enum PopularSince {
+        ONE_HOUR, DAY, WEEK
+    }
+
     Tag findByUuid(UUID uuid) throws TagDoesntExistException;
     Tag findByName(String name) throws TagDoesntExistException;
+    List<Tag> findMostPopular(Long limit, PopularSince since) throws IllegalArgumentException;
 }

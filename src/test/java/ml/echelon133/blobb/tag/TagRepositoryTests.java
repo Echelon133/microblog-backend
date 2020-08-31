ed8592fd8887f75c68f37638317faa81cb740441
Echelon133 <echelon133@gmail.com>
2020-08-31 22:02:37 +0200

    Implement tests of findMostPopularTags_Between

--- src/test/java/ml/echelon133/blobb/tag/TagRepositoryTests.java
+++ src/test/java/ml/echelon133/blobb/tag/TagRepositoryTests.java
@@ -1,11 +1,18 @@
 package ml.echelon133.blobb.tag;
 
+import ml.echelon133.blobb.blobb.Blobb;
+import ml.echelon133.blobb.blobb.BlobbRepository;
+import ml.echelon133.blobb.user.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.neo4j.DataNeo4jTest;
 
-import java.util.Optional;
+import java.time.Instant;
+import java.util.*;
+import java.util.stream.Collectors;
 
+import static java.time.temporal.ChronoUnit.DAYS;
+import static java.time.temporal.ChronoUnit.HOURS;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataNeo4jTest
@@ -13,9 +20,13 @@ public class TagRepositoryTests {
 
     private TagRepository tagRepository;
 
+    private BlobbRepository blobbRepository;
+
     @Autowired
-    public TagRepositoryTests(TagRepository tagRepository) {
+    public TagRepositoryTests(TagRepository tagRepository,
+                              BlobbRepository blobbRepository) {
         this.tagRepository = tagRepository;
+        this.blobbRepository = blobbRepository;
     }
 
     @Test
@@ -35,5 +46,205 @@ public class TagRepositoryTests {
         Optional<Tag> foundTag = tagRepository.findByName("test");
 
         assertEquals(savedTag, foundTag.orElse(null));
+    }
+
+    @Test
+    public void findMostPopularTags_DoesNotCountDeletedBlobbs() {
+        User u1 = new User("u1", "", "", "");
+
+        // create a single blobb tagged '#test' and mark it as deleted
+        Blobb b1 = new Blobb(u1, "test");
+        b1.markAsDeleted();
+        b1.addTag(new Tag("#test"));
+        blobbRepository.save(b1);
+
+        // create another blobb tagged '#asdf' and don't delete it
+        Blobb b2 = new Blobb(u1, "test");
+        b2.addTag(new Tag("#asdf"));
+        blobbRepository.save(b2);
+
+        // when
+        Date hourAgo = Date.from(Instant.now().minus(1, HOURS));
+        List<Tag> popular = tagRepository.findMostPopularTags_Between(hourAgo, new Date(), 10L);
+
+        // then
+        assertEquals(1L, popular.size());
+        List<String> tagNames = popular.stream().map(Tag::getName).collect(Collectors.toList());
+        assertTrue(tagNames.contains("#asdf"));
+    }
+
+    @Test
+    public void findMostPopularTags_CorrectlyCalculatesPopularity() {
+        User u1 = new User("u1", "", "", "");
+
+        Tag test1 = tagRepository.save(new Tag("#test1"));
+        Tag test2 = tagRepository.save(new Tag("#test2"));
+        Tag test3 = tagRepository.save(new Tag("#test3"));
+        Tag test4 = tagRepository.save(new Tag("#test4"));
+
+        // make 100 blobbs tagged '#test1'
+        for (int i = 0; i < 100; i++) {
+            Blobb b = new Blobb(u1, "content");
+            b.addTag(test1);
+            blobbRepository.save(b);
+        }
+
+        // make 200 blobbs tagged '#test2'
+        for (int i = 0; i < 200; i++) {
+            Blobb b = new Blobb(u1, "content");
+            b.addTag(test2);
+            blobbRepository.save(b);
+        }
+
+        // make 300 blobbs tagged '#test3'
+        for (int i = 0; i < 300; i++) {
+            Blobb b = new Blobb(u1, "content");
+            b.addTag(test3);
+            blobbRepository.save(b);
+        }
+
+        // make 400 blobbs tagged '#test4'
+        for (int i = 0; i < 400; i++) {
+            Blobb b = new Blobb(u1, "content");
+            b.addTag(test4);
+            blobbRepository.save(b);
+        }
+
+        // when
+        Date hourAgo = Date.from(Instant.now().minus(1, HOURS));
+        List<Tag> popular = tagRepository.findMostPopularTags_Between(hourAgo, new Date(), 10L);
+
+        // then
+        assertEquals(4, popular.size());
+
+        List<String> tagNames = popular.stream().map(Tag::getName).collect(Collectors.toList());
+        List<String> expectedOrder = Arrays.asList("#test4", "#test3", "#test2", "#test1");
+
+        for (int i = 0; i < popular.size(); i++) {
+            assertEquals(expectedOrder.get(i), tagNames.get(i));
+        }
+    }
+
+    @Test
+    public void findMostPopularTags_IgnoresBlobbsNotCreatedBetweenDates() {
+        Date hourAgo = Date.from(Instant.now().minus(1, HOURS));
+        Date twoHoursAgo = Date.from(Instant.now().minus(2, HOURS));
+        Date weekAgo = Date.from(Instant.now().minus(7, DAYS));
+        Date twoWeeksAgo = Date.from(Instant.now().minus(14, DAYS));
+
+        User u1 = new User("u1", "", "", "");
+
+        Tag test1 = tagRepository.save(new Tag("#test1"));
+        Tag test2 = tagRepository.save(new Tag("#test2"));
+        Tag test3 = tagRepository.save(new Tag("#test3"));
+        Tag test4 = tagRepository.save(new Tag("#test4"));
+
+        // make 100 blobbs tagged '#test1' (created now)
+        for (int i = 0; i < 100; i++) {
+            Blobb b = new Blobb(u1, "content");
+            b.addTag(test1);
+            blobbRepository.save(b);
+        }
+
+        // make 200 blobbs tagged '#test2' (created two hours ago)
+        for (int i = 0; i < 200; i++) {
+            Blobb b = new Blobb(u1, "content");
+            b.setCreationDate(twoHoursAgo);
+            b.addTag(test2);
+            blobbRepository.save(b);
+        }
+
+        // make 300 blobbs tagged '#test3' (created two hours ago)
+        for (int i = 0; i < 300; i++) {
+            Blobb b = new Blobb(u1, "content");
+            b.setCreationDate(twoHoursAgo);
+            b.addTag(test3);
+            blobbRepository.save(b);
+        }
+
+        // make 400 blobbs tagged '#test4' (created a week ago)
+        for (int i = 0; i < 400; i++) {
+            Blobb b = new Blobb(u1, "content");
+            b.setCreationDate(weekAgo);
+            b.addTag(test4);
+            blobbRepository.save(b);
+        }
+
+        // when
+        // popular in the last hour
+        List<Tag> popular1 = tagRepository.findMostPopularTags_Between(hourAgo, new Date(), 10L);
+        // popular between two weeks ago and one hour ago
+        List<Tag> popular2 = tagRepository.findMostPopularTags_Between(twoWeeksAgo, hourAgo, 10L);
+
+        // then
+        // only one tag has been used in the last hour
+        assertEquals(1, popular1.size());
+        // three tags have been used in the second case
+        assertEquals(3, popular2.size());
+
+        List<String> tagNames1 = popular1.stream().map(Tag::getName).collect(Collectors.toList());
+        List<String> tagNames2 = popular2.stream().map(Tag::getName).collect(Collectors.toList());
+
+        List<String> expectedOrder1 = Collections.singletonList("#test1");
+        List<String> expectedOrder2 = Arrays.asList("#test4", "#test3", "#test2");
+
+        for (int i = 0; i < popular1.size(); i++) {
+            assertEquals(expectedOrder1.get(i), tagNames1.get(i));
+        }
+        for (int i = 0; i < popular2.size(); i++) {
+            assertEquals(expectedOrder2.get(i), tagNames2.get(i));
+        }
+    }
+
+    @Test
+    public void findMostPopularTags_LimitsNumberOfResults() {
+        User u1 = new User("u1", "", "", "");
+
+        Tag test1 = tagRepository.save(new Tag("#test1"));
+        Tag test2 = tagRepository.save(new Tag("#test2"));
+        Tag test3 = tagRepository.save(new Tag("#test3"));
+        Tag test4 = tagRepository.save(new Tag("#test4"));
+
+        // make 100 blobbs tagged '#test1'
+        for (int i = 0; i < 100; i++) {
+            Blobb b = new Blobb(u1, "content");
+            b.addTag(test1);
+            blobbRepository.save(b);
+        }
+
+        // make 200 blobbs tagged '#test2'
+        for (int i = 0; i < 200; i++) {
+            Blobb b = new Blobb(u1, "content");
+            b.addTag(test2);
+            blobbRepository.save(b);
+        }
+
+        // make 300 blobbs tagged '#test3'
+        for (int i = 0; i < 300; i++) {
+            Blobb b = new Blobb(u1, "content");
+            b.addTag(test3);
+            blobbRepository.save(b);
+        }
+
+        // make 400 blobbs tagged '#test4'
+        for (int i = 0; i < 400; i++) {
+            Blobb b = new Blobb(u1, "content");
+            b.addTag(test4);
+            blobbRepository.save(b);
+        }
+
+        // when
+        Date hourAgo = Date.from(Instant.now().minus(1, HOURS));
+        List<Tag> popular = tagRepository.findMostPopularTags_Between(hourAgo, new Date(), 2L);
+
+        // then
+        assertEquals(2, popular.size());
+
+        List<String> tagNames = popular.stream().map(Tag::getName).collect(Collectors.toList());
+        List<String> expectedOrder = Arrays.asList("#test4", "#test3");
+
+        for (int i = 0; i < popular.size(); i++) {
+            assertEquals(expectedOrder.get(i), tagNames.get(i));
+        }
     }
 }

a5f650c06180d7b8f17ec2254354db637a3eb06f
Echelon133 <echelon133@gmail.com>
2020-08-31 20:09:03 +0200

    Make BlobbRepository queries ignore blobbs marked as deleted

--- src/main/java/ml/echelon133/blobb/blobb/BlobbRepository.java
+++ src/main/java/ml/echelon133/blobb/blobb/BlobbRepository.java
@@ -19,7 +19,7 @@ public interface BlobbRepository extends Neo4jRepository<Blobb, UUID> {
         results.
      */
     @Query( "MATCH (u:User)-[:FOLLOWS]->(poster:User)-[:POSTS]->(blobbs:Blobb) " +
-            "WHERE u.uuid = $uuid AND blobbs.creationDate >= $first AND blobbs.creationDate <= $second " +
+            "WHERE u.uuid = $uuid AND blobbs.creationDate >= $first AND blobbs.creationDate <= $second AND blobbs.deleted <> true " +
             "OPTIONAL MATCH (blobbs:Blobb)-[:RESPONDS]->(respondsTo:Blobb) " +
             "OPTIONAL MATCH (blobbs:Blobb)-[:REBLOBBS]->(reblobbs:Blobb) " +
             "RETURN blobbs.uuid AS uuid, blobbs.content AS content, blobbs.creationDate AS date, poster AS author, " +
@@ -27,29 +27,29 @@ public interface BlobbRepository extends Neo4jRepository<Blobb, UUID> {
             "ORDER BY datetime(blobbs.creationDate) DESC SKIP $skip LIMIT $limit ")
     List<FeedBlobb> getFeedForUserWithUuid_PostedBetween(UUID uuid, Date first, Date second, Long skip, Long limit);
 
-    @Query( "MATCH (u:User)-[:POSTS]->(blobb:Blobb) WHERE blobb.uuid = $uuid " +
+    @Query( "MATCH (u:User)-[:POSTS]->(blobb:Blobb) WHERE blobb.uuid = $uuid AND blobb.deleted <> true " +
             "OPTIONAL MATCH (blobb:Blobb)-[:RESPONDS]->(respondsTo:Blobb) " +
             "OPTIONAL MATCH (blobb:Blobb)-[:REBLOBBS]->(reblobbs:Blobb) " +
             "RETURN blobb.uuid AS uuid, blobb.content AS content, blobb.creationDate AS date, u AS author, " +
             "reblobbs.uuid AS reblobbs, respondsTo.uuid AS respondsTo ")
     Optional<FeedBlobb> getBlobbWithUuid(UUID uuid);
 
-    @Query( "MATCH (blobb:Blobb) WHERE blobb.uuid = $uuid " +
+    @Query( "MATCH (blobb:Blobb) WHERE blobb.uuid = $uuid AND blobb.deleted <> true " +
             "OPTIONAL MATCH (:User)-[likes:LIKES]->(blobb:Blobb) " +
-            "OPTIONAL MATCH (:ResponseBlobb)-[responses:RESPONDS]->(blobb) " +
-            "OPTIONAL MATCH (:Reblobb)-[reblobbs:REBLOBBS]->(blobb:Blobb) " +
+            "OPTIONAL MATCH (res:ResponseBlobb)-[responses:RESPONDS]->(blobb) WHERE res.deleted <> true " +
+            "OPTIONAL MATCH (reb:Reblobb)-[reblobbs:REBLOBBS]->(blobb:Blobb) WHERE reb.deleted <> true " +
             "RETURN blobb.uuid AS uuid, count(distinct(responses)) AS responses, count(distinct(likes)) AS likes, " +
             "count(distinct(reblobbs)) AS reblobbs")
     Optional<BlobbInfo> getInfoAboutBlobbWithUuid(UUID uuid);
 
     @Query( "MATCH (u:User) WHERE u.uuid = $uuidOfUser " +
-            "MATCH (b:Blobb) WHERE b.uuid = $uuidOfBlobb " +
+            "MATCH (b:Blobb) WHERE b.uuid = $uuidOfBlobb AND b.deleted <> true " +
             "CREATE (u)-[l:LIKES]->(b) " +
             "RETURN id(l)")
     Optional<Long> likeBlobbWithUuid(UUID uuidOfUser, UUID uuidOfBlobb);
 
-    @Query( "MATCH (u:User)-[l:LIKES]->(b:Blobb) " +
-            "WHERE u.uuid = $uuidOfUser AND b.uuid = $uuidOfBlobb " +
+    @Query( "MATCH (u:User)-[l:LIKES]->(b:Blobb)" +
+            "WHERE u.uuid = $uuidOfUser AND b.uuid = $uuidOfBlobb AND b.deleted <> true " +
             "RETURN id(l)")
     Optional<Long> checkIfUserWithUuidLikes(UUID uuidOfUser, UUID uuidOfBlobb);
 
@@ -58,15 +58,19 @@ public interface BlobbRepository extends Neo4jRepository<Blobb, UUID> {
             "DELETE l")
     void unlikeBlobbWithUuid(UUID uuidOfUser, UUID uuidOfBlobb);
 
+    // allow listing responses to blobbs marked as deleted
+    // but dont list responses that are marked as deleted
     @Query( "MATCH (blobb:Blobb) WHERE blobb.uuid = $uuid " +
-            "MATCH (u:User)-[:POSTS]->(response:ResponseBlobb)-[:RESPONDS]->(blobb) " +
+            "MATCH (u:User)-[:POSTS]->(response:ResponseBlobb)-[:RESPONDS]->(blobb) WHERE response.deleted <> true " +
             "RETURN response.uuid AS uuid, response.content AS content, response.creationDate AS date, u AS author, " +
             "NULL AS reblobbs, blobb.uuid AS respondsTo " +
             "ORDER BY date ASC SKIP $skip LIMIT $limit")
     List<FeedBlobb> getAllResponsesToBlobbWithUuid(UUID uuid, Long skip, Long limit);
 
+    // allow listing reblobbs even when referenced blobb is marked as deleted
+    // but don't list reblobbs that are marked as deleted
     @Query( "MATCH (blobb:Blobb) WHERE blobb.uuid = $uuid " +
-            "MATCH (u:User)-[:POSTS]->(reblobb:Reblobb)-[:REBLOBBS]->(blobb) " +
+            "MATCH (u:User)-[:POSTS]->(reblobb:Reblobb)-[:REBLOBBS]->(blobb) WHERE reblobb.deleted <> true " +
             "RETURN reblobb.uuid AS uuid, reblobb.content AS content, reblobb.creationDate AS date, u AS author, " +
             "blobb.uuid AS reblobbs, NULL AS respondsTo " +
             "ORDER BY date ASC SKIP $skip LIMIT $limit")

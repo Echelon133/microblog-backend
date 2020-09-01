cdb4238004a5cbf8d65f8585ced0581992b9cdc9
Echelon133 <echelon133@gmail.com>
2020-09-01 22:20:54 +0200

    Implement query that finds most recent posts with a specific tag

--- src/main/java/ml/echelon133/blobb/tag/TagRepository.java
+++ src/main/java/ml/echelon133/blobb/tag/TagRepository.java
@@ -15,4 +15,13 @@ public interface TagRepository extends Neo4jRepository<Tag, UUID> {
             "WHERE b.creationDate >= $first AND b.creationDate <= $second AND b.deleted <> true " +
             "WITH t, count(r) as tagCounter RETURN t ORDER BY tagCounter DESC LIMIT $howManyTagsToGet")
     List<Tag> findMostPopularTags_Between(Date first, Date second, Long howManyTagsToGet);
+
+    @Query( "MATCH (t:Tag)-[:TAGS]->(b:Blobb)<-[:POSTS]-(u:User) " +
+            "WHERE t.uuid = $tagUuid AND b.deleted <> true " +
+            "OPTIONAL MATCH (b:Blobb)-[:RESPONDS]->(respondsTo:Blobb) " +
+            "OPTIONAL MATCH (b:Blobb)-[:REBLOBBS]->(reblobbs:Blobb) " +
+            "RETURN b.uuid AS uuid, b.content AS content, b.creationDate AS date, u AS author, " +
+            "reblobbs.uuid AS reblobbs, respondsTo.uuid AS respondsTo " +
+            "ORDER BY datetime(b.creationDate) DESC SKIP $skip LIMIT $limit ")
+    List<RecentBlobb> findRecentBlobbsTagged(UUID tagUuid, Long skip, Long limit);
 }

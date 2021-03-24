08d960cbd00e07aec87384a4577820a686eddd3f
Mateusz <echelon133@gmail.com>
2021-03-24 19:37:35 +0100

    Configure redis connection factory

--- src/main/java/ml/echelon133/microblog/MicroblogApplication.java
+++ src/main/java/ml/echelon133/microblog/MicroblogApplication.java
@@ -1,8 +1,14 @@
 package ml.echelon133.microblog;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
@@ -11,6 +17,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Objects;
 
 @SpringBootApplication
 public class MicroblogApplication {
@@ -29,6 +36,26 @@ public class MicroblogApplication {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
+	}
+
+	@Configuration
+	public static class RedisConfig {
+
+		private final Environment env;
+
+		@Autowired
+		public RedisConfig(Environment env) {
+			this.env = env;
+		}
+
+		@Bean
+		public JedisConnectionFactory jedisConnectionFactory() {
+			String password = Objects.requireNonNull(env.getProperty("spring.redis.password"));
+
+			RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+			config.setPassword(RedisPassword.of(password));
+			return new JedisConnectionFactory(config);
+		}
 	}
 
 	public static void main(String[] args) {

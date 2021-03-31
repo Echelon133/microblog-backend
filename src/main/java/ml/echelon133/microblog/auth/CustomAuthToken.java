01c25989ce1a3c1e2fd24b433cfc3aa07e4c7171
Mateusz <echelon133@gmail.com>
2021-04-01 00:25:52 +0200

    Use UserPrincipal to make User and CustomAuthToken.User interchangeable

--- src/main/java/ml/echelon133/microblog/auth/CustomAuthToken.java
+++ src/main/java/ml/echelon133/microblog/auth/CustomAuthToken.java
@@ -1,18 +1,48 @@
 package ml.echelon133.microblog.auth;
 
-import ml.echelon133.microblog.user.User;
+import ml.echelon133.microblog.user.UserPrincipal;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class CustomAuthToken implements Authentication {
 
     private User user;
     private String accessToken;
 
-    public CustomAuthToken(User user, String accessToken) {
-        this.user = user;
+    public static class User implements UserPrincipal {
+        private UUID uuid;
+        private String username;
+        private List<GrantedAuthority> authorities;
+
+        public User(UUID uuid, String username, List<String> r) {
+            this.uuid = uuid;
+            this.username = username;
+            this.authorities = r
+                    .stream()
+                    .map(role -> (GrantedAuthority) () -> role)
+                    .collect(Collectors.toList());
+        }
+
+        public UUID getUuid() {
+            return uuid;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public List<GrantedAuthority> getAuthorities() {
+            return authorities;
+        }
+    }
+
+    public CustomAuthToken(UUID uuid, String username, List<String> r, String accessToken) {
+        this.user = new User(uuid, username, r);
         this.accessToken = accessToken;
     }
 

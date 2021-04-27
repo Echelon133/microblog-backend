ae530c9a1bb60321f552be546005bc99ab3f7b9e
Mateusz <echelon133@gmail.com>
2021-04-27 21:22:06 +0200

    Add MentionNotification, that represents a 'person mentioned you in their post' type notification

---
+++ src/main/java/ml/echelon133/microblog/notification/MentionNotification.java
@@ -0,0 +1,13 @@
+package ml.echelon133.microblog.notification;
+
+import ml.echelon133.microblog.post.Post;
+import ml.echelon133.microblog.user.User;
+import org.neo4j.ogm.annotation.RelationshipEntity;
+
+@RelationshipEntity(type = "NOTIFIES")
+public class MentionNotification extends Notification {
+
+    public MentionNotification(Post post, User user) {
+        super("mention", post, user);
+    }
+}

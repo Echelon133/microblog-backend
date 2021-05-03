36db1960ded824611f88dd339c7fbcab4877d17f
Mateusz <echelon133@gmail.com>
2021-05-03 22:51:34 +0200

    Declare ReportService interface

---
+++ src/main/java/ml/echelon133/microblog/report/IReportService.java
@@ -0,0 +1,11 @@
+package ml.echelon133.microblog.report;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface IReportService {
+    List<ReportResult> findAllReports(Long skip, Long limit, boolean checked) throws IllegalArgumentException;
+    boolean createNewReport(UUID reportingUserUuid, UUID reportedPostUuid, String reason, String description)
+            throws ResourceDoesNotExistException, IllegalArgumentException;
+    boolean checkReport(UUID reportUuid, boolean acceptReport);
+}

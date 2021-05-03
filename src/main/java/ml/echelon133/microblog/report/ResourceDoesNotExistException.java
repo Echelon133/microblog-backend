df0bef777e6e29260480042a27a550814ead0b42
Mateusz <echelon133@gmail.com>
2021-05-04 00:02:53 +0200

    Add exception classes

---
+++ src/main/java/ml/echelon133/microblog/report/ResourceDoesNotExistException.java
@@ -0,0 +1,8 @@
+package ml.echelon133.microblog.report;
+
+public class ResourceDoesNotExistException extends Exception {
+
+    public ResourceDoesNotExistException(String message) {
+        super(message);
+    }
+}

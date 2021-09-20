3e154f0c08b4d1083c02a3bdc84cb51ad896bdf2
Mateusz <echelon133@gmail.com>
2021-09-20 21:23:39 +0200

    Add a new exception that should be used when user tries to modify their state that shouldn't be modifiable

---
+++ src/main/java/ml/echelon133/microblog/user/exception/HiddenStateModificationAttemptException.java
@@ -0,0 +1,8 @@
+package ml.echelon133.microblog.user.exception;
+
+public class HiddenStateModificationAttemptException extends Exception {
+
+    public HiddenStateModificationAttemptException(String message) {
+        super(message);
+    }
+}
